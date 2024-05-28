f4938819745284495ea7f9ae19a66bf2ad45c861
arnabjoy123 <arnab99joy@gmail.com>
2024-05-28 23:05:11 +0530

    intial commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+export default function Github() {
+
+
+  const [data,setData] = useState([])
+
+  useEffect(()=>{
+    fetch('https://api.github.com/users/arnabjoy123')
+    .then((response)=>response.json())
+    .then(data=>{
+      console.log(data)
+      setData(data)
+    })
+  },[])
+
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+        <img src={data.avatar_url} width={300}/>
+
+    </div>
+
+  )
+}
+
+
+
+ 
